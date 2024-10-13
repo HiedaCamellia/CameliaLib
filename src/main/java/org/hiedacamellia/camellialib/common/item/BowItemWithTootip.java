75b529a981b05ab6ae476c725d3693db73467655
Yuqi154 <1123489751@qq.com>
2024-10-13 11:57:58 +0800

    0.0.4 将类移入lib

---
+++ src/main/java/org/hiedacamellia/camellialib/common/item/BowItemWithTootip.java
@@ -0,0 +1,33 @@
+package org.hiedacamellia.camellialib.common.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class BowItemWithTootip extends BowItem {
+    public BowItemWithTootip(Properties properties) {
+        super(properties);
+    }
+    @Override
+    public void appendHoverText(@NotNull ItemStack itemstack, Item.@NotNull TooltipContext context, @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+        super.appendHoverText(itemstack, context, list, flag);
+        ResourceLocation key = BuiltInRegistries.ITEM.getKey(itemstack.getItem());
+        if (!Screen.hasShiftDown()) {
+            list.add(Component.translatable("tooltip.camellialib.press_shift").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+        } else {
+            String[] description = Component.translatable(key.toLanguageKey("item")+".desc").getString().split("§n");
+            for (String line : description) {
+                list.add(Component.literal(line));
+            }
+        }
+    }
+}
